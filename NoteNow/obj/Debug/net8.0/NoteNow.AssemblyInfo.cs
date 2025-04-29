5ee844ed860531e951bdb06914b1e669304946e8
Peter.Chiu <tidub76217@hotmail.com>
2025-04-29 11:17:59 +0800

    Add GitHub Actions workflow for .NET Core

--- NoteNow/obj/Debug/net8.0/NoteNow.AssemblyInfo.cs
+++ NoteNow/obj/Debug/net8.0/NoteNow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteNow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0556c3e7ca3fa17f9c5a823432080e9017489b18")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteNow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteNow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
