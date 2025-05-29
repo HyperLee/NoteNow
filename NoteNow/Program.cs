dbfa62afba3203b765792a0712474e6abc795b52
Peter.Chiu <tidub76217@hotmail.com>
2025-05-29 16:12:22 +0800

    feat: 更新 Program.cs 以實現 MaxTargetNodes 方法，並新增測試資料

--- NoteNow/Program.cs
+++ NoteNow/Program.cs
@@ -6,59 +6,99 @@ namespace NoteNow;
 class Program
 {
     /// <summary>
-    /// 解題 1. Two Sum
-    /// 題目連結：https://leetcode.com/problems/two-sum/
     /// 
-    /// 題目描述：
-    /// 給定一個整數陣列 nums 和一個目標值 target，請找出陣列中和為目標值的兩個數字，並回傳它們的索引。
-    /// 假設每個輸入只會對應一組答案，但同一個元素不能重複使用。
     /// </summary>
-    /// <param name="nums">輸入整數陣列</param>
-    /// <param name="target">目標值</param>
-    /// <returns>和為目標值的兩個數字的索引陣列</returns>
-    public static int[] TwoSum(int[] nums, int target)
-    {
-        // 建立一個字典（Dictionary）來儲存數字及其對應的索引
-        Dictionary<int, int> numDict = new Dictionary<int, int>();
-        // 逐一遍歷陣列中的每個元素
-        for (int i = 0; i < nums.Length; i++)
-        {
-            // 計算目前元素的補數（目標值減去目前數字）
-            int complement = target - nums[i];
-            // 檢查補數是否已經存在於字典中
-            if (numDict.ContainsKey(complement))
-            {
-                // 若存在，代表找到一組解，回傳補數的索引與目前索引
-                return new int[] { numDict[complement], i };
-            }
-            // 若補數不存在，將目前數字與索引加入字典
-            if (!numDict.ContainsKey(nums[i]))
-            {
-                numDict[nums[i]] = i;
-            }
-        }
-        // 若遍歷完畢仍無解，回傳空陣列
-        return Array.Empty<int>();
-    }
-
+    /// <param name="args"></param> <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
-        // 測試資料組 1
-        int[] nums1 = { 2, 7, 11, 15 };
-        int target1 = 9;
-        int[] result1 = TwoSum(nums1, target1);
-        Console.WriteLine("測試資料組 1 TwoSum 結果: [" + string.Join(", ", result1) + "]");
+                // 測試資料：
+        // Tree1: 0-1-2
+        // Tree2: 0-1
+        // k = 2
+        int[][] edges1 = new int[][]
+        {
+            new int[] { 0, 1 },
+            new int[] { 1, 2 }
+        };
+        int[][] edges2 = new int[][]
+        {
+            new int[] { 0, 1 }
+        };
+        int k = 2;
 
-        // 測試資料組 2
-        int[] nums2 = { 3, 2, 4 };
-        int target2 = 6;
-        int[] result2 = TwoSum(nums2, target2);
-        Console.WriteLine("測試資料組 2 TwoSum 結果: [" + string.Join(", ", result2) + "]");
+        // 解法1
+        var prog = new Program();
+        int[] ans1 = prog.MaxTargetNodes(edges1, edges2, k);
+        Console.WriteLine("解法1結果: [" + string.Join(", ", ans1) + "]");
+    }
 
-        // 測試資料組 3
-        int[] nums3 = { 3, 3 };
-        int target3 = 6;
-        int[] result3 = TwoSum(nums3, target3);
-        Console.WriteLine("測試資料組 3 TwoSum 結果: [" + string.Join(", ", result3) + "]");
+    public int[] MaxTargetNodes(int[][] edges1, int[][] edges2, int k)
+    {
+        int n = edges1.Length + 1;
+        int m = edges2.Length + 1;
+        int[] count1 = Build(edges1, k);
+        int[] count2 = Build(edges2, k - 1);
+        int maxCount2 = 0;
+        foreach (var c in count2)
+        {
+            if (c > maxCount2)
+            {
+                maxCount2 = c;
+            }
+        }
+
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            res[i] = count1[i] + maxCount2;
+        }
+        
+        return res;
+    }
+
+    private int[] Build(int[][] edges, int k)
+    {
+        int n = edges.Length + 1;
+        List<List<int>> children = new List<List<int>>(n);
+        for (int i = 0; i < n; i++)
+        {
+            children.Add(new List<int>());
+        }
+
+        foreach (var edge in edges)
+        { 
+            int a = edge[0];
+            int b = edge[1];
+            children[a].Add(b);
+            children[b].Add(a);
+        }
+
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            res[i] = Dfs(i, -1, children, k);
+        }
+        return res;
+    }
+
+    private int Dfs(int node, int parent, List<List<int>> children, int k)
+    {
+        if (k < 0)
+        {
+            return 0;
+        }
+
+        int count = 1;
+        foreach (var child in children[node])
+        {
+            if (child != parent)
+            {
+                count += Dfs(child, node, children, k - 1);
+            }
+        }
+        return count;
     }
 }
